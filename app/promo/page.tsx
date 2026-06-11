1b0c163f8dbd7bd7f838510a1345a8ff113a5e11
jamesjohnsonengland1-eng <jamesjohnsonengland1@gmail.com>
2026-06-11 11:57:56 +0200

    feat: scaffold Next.js promo app

---
+++ app/promo/page.tsx
@@ -0,0 +1,9 @@
+import PocketDebatePromoSequence from "@/components/PocketDebatePromoSequence";
+
+export default function PromoPage() {
+  return (
+    <main style={{ background: "#0F172A", minHeight: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+      <PocketDebatePromoSequence />
+    </main>
+  );
+}
